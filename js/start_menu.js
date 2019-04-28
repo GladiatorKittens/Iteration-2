848baa501d4dde94a009598200f1f27f4f9dc6c0
GladiatorKittens <43640306+GladiatorKittens@users.noreply.github.com>
2019-04-28 01:25:47 +0100

    Created Tutorial

--- js/start_menu.js
+++ js/start_menu.js
@@ -1,16 +1,32 @@
 class StartScreen extends Phaser.Scene {
     constructor() {
         super("StartScreen");
-        //this.id = "StartScreen";
+        this.tutorial_visible = false;
     }
     preload() {
         this.load.image("start_button", "assets/art/Buttons/start_button.png");
         this.load.image("tutorial_button", "assets/art/Buttons/tutorial_button.png")
+        this.load.image("tutorial", "assets/other/rsz_tutorial.png");
     }
     create() {
         var start_button = new Button(450, 250, "start_button", function () { this.scene.start("Level_1") }, this);
         //this.tutorial_image
-        var tutorial_button = new Button(450, 350, "tutorial_button", function() {/*load tutorial*/ }, this);
+        var tutorial_button = new Button(450, 350, "tutorial_button", function () {
+            this.tutorial.image.setActive(true);
+            this.tutorial.image.setVisible(true);
+            this.tutorial_visible = true;
+        }, this);
+
+        this.tutorial = new Button(480, 272, "tutorial", function () {
+            this.tutorial.image.setActive(false);
+            this.tutorial.image.setVisible(false);
+            this.tutorial_visible = false;
+        }, this);
+        this.tutorial.image.setActive(false);
+        this.tutorial.image.setVisible(false);
+    }
+    update() {
+        
     }
 }
 class GameOver extends Phaser.Scene {
